656b42f4e201df9c703bfc9a7d4dd97b26dde5f8
ZXstrikes <strike.miner0001@gmail.com>
2023-11-16 23:09:02 +0700

    .

--- lib/views/screens/home_screen/home_screen.dart
+++ lib/views/screens/home_screen/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:harsa_mobile/utils/constants/colors.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,34 +12,79 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          Container(),
-          SingleChildScrollView(
-            child: Column(
-              children: [
-                const Text('Kategori'),
-                GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 4,
-                  ),
-                  itemCount: 8,
-                  itemBuilder: (context, index) => Container(
-                    height: 84,
-                    width: 84,
-                    alignment: Alignment.center,
-                    child: InkWell(
-                      onTap: () {},
-                      child: const Text('Kategori'),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: ColorsPallete.whiteGrey,
+        body: Column(
+          children: [
+            Container(),
+            SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: SizedBox(
+                              height: 50,
+                              child: SearchBar(
+                                elevation: const MaterialStatePropertyAll(0),
+                                side: const MaterialStatePropertyAll(
+                                  BorderSide(color: Colors.grey, width: 1),
+                                ),
+                                padding: const MaterialStatePropertyAll(
+                                  EdgeInsets.symmetric(
+                                    horizontal: 10,
+                                    vertical: 0,
+                                  ),
+                                ),
+                                trailing: [
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 5),
+                                    child: SvgPicture.asset(
+                                      'assets/icons/outline/magnifyingglass.svg',
+                                      colorFilter: const ColorFilter.mode(
+                                        Colors.grey,
+                                        BlendMode.srcIn,
+                                      ),
+                                      height: 20,
+                                      width: 20,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          const CircleAvatar(
+                            radius: 25,
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                )
-              ],
-            ),
-          )
-        ],
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    SizedBox(
+                      height: 220,
+                      width: double.infinity,
+                      child: Image.network(''),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
