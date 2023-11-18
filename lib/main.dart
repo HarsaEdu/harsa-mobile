6085f33ea2ce221b85f143c96e4ccf005b6242ea
AgentGalon <zahranrafif911@gmail.com>
2023-11-18 15:03:23 +0700

    fix conflict

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:harsa_mobile/viewmodels/onboarding_viewmodel.dart';
-import 'package:harsa_mobile/viewmodels/splash_viewmodel.dart';
+import 'package:provider/provider.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 import 'package:harsa_mobile/views/screens/InterestScreen/interest_category_screen.dart';
 import 'package:harsa_mobile/views/screens/onboarding_screen/onboarding_screen.dart';
 import 'package:harsa_mobile/views/screens/signup_screen/signup_screen.dart';
 import 'package:harsa_mobile/views/screens/signup_screen/signupdata_screen.dart';
 import 'package:harsa_mobile/views/screens/splash_screen/splash_screen.dart';
-import 'package:provider/provider.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:harsa_mobile/views/screens/login_screen/login_screen.dart';
+import 'package:harsa_mobile/views/screens/main_screen/main_screen.dart';
+import 'package:harsa_mobile/views/screens/faq_screen/faq_screen.dart';
+import 'package:harsa_mobile/views/screens/notification_screen/notification_screen.dart';
+
+import 'package:harsa_mobile/viewmodels/onboarding_viewmodel.dart';
+import 'package:harsa_mobile/viewmodels/splash_viewmodel.dart';
 import 'package:harsa_mobile/viewmodels/login_provider.dart';
-import 'package:harsa_mobile/views/screens/login_screen.dart';
 import 'package:harsa_mobile/viewmodels/intereset_provider.dart';
 import 'package:harsa_mobile/viewmodels/signup_provider.dart';
 import 'package:harsa_mobile/viewmodels/signupdata_provider.dart';
 import 'package:harsa_mobile/viewmodels/inbox_provider.dart';
 import 'package:harsa_mobile/viewmodels/main_screen_provider.dart';
-import 'package:harsa_mobile/views/screens/main_screen/main_screen.dart';
-import 'package:harsa_mobile/views/screens/faq_screen/faq_screen.dart';
-import 'package:harsa_mobile/views/screens/notification_screen/notification_screen.dart';
 import 'package:harsa_mobile/viewmodels/faq_screen_provider.dart';
 
 void main() {
