68ee917a0a0398a27c041665712d46fd69951037
zenxiety <andyanyoga22@gmail.com>
2023-12-18 13:54:39 +0700

    fix - feedback null & overflows

--- lib/views/screens/home_screen/home_screen.dart
+++ lib/views/screens/home_screen/home_screen.dart
@@ -264,11 +264,13 @@ class _HomeScreenState extends State<HomeScreen> {
                                         margin:
                                             const EdgeInsets.only(right: 26),
                                         width: 286,
-                                        child: Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
+                                        child: ListView(
+                                          // mainAxisAlignment:
+                                          //     MainAxisAlignment.center,
+                                          // crossAxisAlignment:
+                                          //     CrossAxisAlignment.start,
+                                          physics:
+                                              const NeverScrollableScrollPhysics(),
                                           children: [
                                             Image.network(
                                               recommendation.courseImage,
@@ -416,12 +418,14 @@ class _HomeScreenState extends State<HomeScreen> {
                                                   horizontal: 15,
                                                   vertical: 18,
                                                 ),
-                                                child: Column(
-                                                  mainAxisAlignment:
-                                                      MainAxisAlignment
-                                                          .spaceBetween,
-                                                  crossAxisAlignment:
-                                                      CrossAxisAlignment.start,
+                                                child: ListView(
+                                                  // mainAxisAlignment:
+                                                  //     MainAxisAlignment
+                                                  //         .spaceBetween,
+                                                  // crossAxisAlignment:
+                                                  //     CrossAxisAlignment.start,
+                                                  physics:
+                                                      const NeverScrollableScrollPhysics(),
                                                   children: [
                                                     Text(
                                                       subsPlan.title,
